c7f2fcd31f53b3c989973e510fd8d97a7669d905
Shaaf, Syed <sshaaf@redhat.com>
2023-01-25 13:49:58 +0100

    add quarkus crud backend files

---
+++ quarkus/crud-oidc/src/main/java/org/acme/BookResource.java
@@ -0,0 +1,71 @@
+package org.acme;
+
+import org.jboss.resteasy.reactive.NoCache;
+
+import javax.annotation.security.RolesAllowed;
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+import java.util.List;
+
+@Path("/books")
+public class BookResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @RolesAllowed("user")
+    @NoCache
+    public List<Book> getAll() {
+        return Book.listAll();
+
+    }
+    
+    @GET
+    @Path("/{isbn}")
+    @RolesAllowed("user")
+    @NoCache
+    public Book getOne(@PathParam("isbn") String isbn) {
+        Book entity = Book.findById(isbn);
+        if (entity == null) {
+            throw new WebApplicationException("Book with id of " + isbn + " does not exist.", Response.Status.NOT_FOUND);
+        }
+        return entity;
+    }
+
+    @POST
+    @RolesAllowed("admin")
+    @NoCache
+    @Transactional
+    public Response create(@Valid Book item) {
+        item.persist();
+        return Response.status(Response.Status.CREATED).entity(item).build();
+    }
+
+    @PATCH
+    @Path("/{id}")
+    @RolesAllowed("admin")
+    @NoCache
+    @Transactional
+    public Response update(@Valid Book book, @PathParam("id") String isbn) {
+        Book entity = Book.findById(isbn);
+        entity.title = book.title;
+        entity.genre = book.genre;
+        entity.summary = book.summary;
+        return Response.ok(entity).build();
+    }
+
+    @DELETE
+    @Path("/{isbn}")
+    @RolesAllowed("admin")
+    @NoCache
+    @Transactional
+    public Response deleteOne(@PathParam("isbn") String isbn) {
+        Book entity = Book.findById(isbn);
+        if (entity == null) {
+            throw new WebApplicationException("Book with isbn of " + isbn + " does not exist.", Response.Status.NOT_FOUND);
+        }
+        entity.delete();
+        return Response.noContent().build();
+    }
+}
